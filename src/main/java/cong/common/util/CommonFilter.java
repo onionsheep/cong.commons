0ed1abbe1497830edf57e40b044fe71289022cbb
刘聪 <onion_sheep@163.com>
2014-12-27 23:00:56 +0800

    给CommonFilter添加注释，增加一点字符编码设置功能
    Signed-off-by: 刘聪 <onion_sheep@163.com>

--- src/main/java/cong/common/util/CommonFilter.java
+++ src/main/java/cong/common/util/CommonFilter.java
@@ -1,97 +1,104 @@
 package cong.common.util;
 
+import jodd.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.UnsupportedCharsetException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
- * Servlet Filter implementation class RouterFilter
+ * 把contextPath设置到ServletContext中<br/>
+ * request中的参数列表parameterMap设置到request的parameterMap属性中，此参数的值为String[]<br/>
+ * 把parameterMap中的值的第一个拿出来，形成一个单一值的paramMap，并设置到request的paramMap属性中<br/>
+ * 读取初始化参数defaultCharset，并设置编码，若为空，则默认为UTF-8
  */
 public class CommonFilter implements Filter {
 
-  private static final Logger log = LoggerFactory.getLogger(CommonFilter.class);
+    private static final Logger log = LoggerFactory.getLogger(CommonFilter.class);
 
-  private static Pattern nologURLPattern = Pattern.compile("^(.+[.])(png|gif|jpg|js(?!p)|css|jpeg)");
+    private String defaultCharset = "UTF-8";
 
-  /**
-   * Default constructor.
-   */
-  public CommonFilter() {
+    public CommonFilter() {
 
-  }
-
-  /**
-   * @see Filter#destroy()
-   */
-  @Override
-  public void destroy() {
-
-  }
-
-  /**
-   * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-   */
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-    HttpServletRequest req = (HttpServletRequest) request;
-    //HttpServletResponse resp = (HttpServletResponse)response;
-
-    @SuppressWarnings("unchecked")
-    Map<String, String[]> parameterMap = req.getParameterMap();
-    req.setAttribute("parameterMap", parameterMap);
-    Map<String, String> paramMap = new HashMap<String, String>();
-    for (Entry<String, String[]> e : parameterMap.entrySet()) {
-      if (e.getValue().length > 0) {
-        paramMap.put(e.getKey(), e.getValue()[0]);
-      }
     }
-    req.setAttribute("paramMap", paramMap);
-    //String requestURI = req.getRequestURI();
-    //根据uri进行分别处理
 
-    if (log.isDebugEnabled()) {
-      final String remoteHost = request.getRemoteHost();
-      final StringBuffer requestURL = req.getRequestURL();
-      log.debug("{} <= {}", remoteHost, requestURL);
-      int paramSize = parameterMap.entrySet().size();
-      if (paramSize > 0) {
-        StringBuffer sb = new StringBuffer();
+    /**
+     * @see Filter#destroy()
+     */
+    @Override
+    public void destroy() {
+
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse)response;
+
+        req.setCharacterEncoding(defaultCharset);
+        resp.setCharacterEncoding(defaultCharset);
+
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        req.setAttribute("parameterMap", parameterMap);
+        Map<String, String> paramMap = new HashMap<String, String>();
         for (Entry<String, String[]> e : parameterMap.entrySet()) {
-          if (paramSize > 1) {
-            sb.append("\n");
-          }
-          sb.append(e.getKey());
-          sb.append(Arrays.toString(e.getValue()));
+            if (e.getValue().length > 0) {
+                paramMap.put(e.getKey(), e.getValue()[0]);
+            }
         }
-        log.debug("param lists :{}", sb.toString());
-      }
+        req.setAttribute("paramMap", paramMap);
 
-      Matcher matcher = nologURLPattern.matcher(requestURL);
-      boolean matches = matcher.find();
-      if (!matches) {
-        //
-      }
+
+        if (log.isDebugEnabled()) {
+            final String remoteHost = request.getRemoteHost();
+            final StringBuffer requestURL = req.getRequestURL();
+            log.debug("{} <= {}", remoteHost, requestURL);
+            int paramSize = parameterMap.entrySet().size();
+            if (paramSize > 0) {
+                StringBuffer sb = new StringBuffer();
+                for (Entry<String, String[]> e : parameterMap.entrySet()) {
+                    if (paramSize > 1) {
+                        sb.append("\n");
+                    }
+                    sb.append(e.getKey());
+                    sb.append(Arrays.toString(e.getValue()));
+                }
+                log.debug("param lists :{}", sb.toString());
+            }
+        }
+        chain.doFilter(request, response);
     }
 
-    chain.doFilter(request, response);
-
-  }
-
-  /**
-   * @see Filter#init(FilterConfig)
-   */
-  @Override
-  public void init(FilterConfig fConfig) throws ServletException {
-    fConfig.getServletContext().setAttribute("contextPath", fConfig.getServletContext().getContextPath());
-  }
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    @Override
+    public void init(FilterConfig fConfig) throws ServletException {
+        fConfig.getServletContext().setAttribute("contextPath", fConfig.getServletContext().getContextPath());
+        String charset = fConfig.getInitParameter("defaultCharset");
+        if (StringUtil.isBlank(charset)) {
+            charset = "UTF-8";
+        } else {
+            try {
+                Charset.forName(charset);
+            } catch (UnsupportedCharsetException e) {
+                e.printStackTrace();
+                charset = "UTF-8";
+            }
+        }
+        defaultCharset = charset;
+    }
 
 }
